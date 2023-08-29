32a91e699cdc2a034e1d3808039389961a17cec3
jyh2610 <jogg4177@naver.com>
2023-08-29 21:11:17 +0900

    Change npm ,node version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import IssuesList from "./pages/IssuesList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <IssuesList />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/IssuesList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function IssuesList() {
+  return <div>1</div>;
+}
+
+export default IssuesList;
